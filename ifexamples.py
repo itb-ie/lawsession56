2902ce5fe69c8e60d1accd228f222fe62e33f8b3
itb-ie <42895988+itb-ie@users.noreply.github.com>
2021-03-05 18:21:57 +0100

    added if examples

---
+++ ifexamples.py
@@ -0,0 +1,8 @@
+number = input("give me a number please")
+try:
+    number = int(number)
+except:
+    print("that was not a number")
+else:
+    if number % 2 == 0:
+        print("your number is even")
---
+++ morecomplicatedif.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 20
+
+if number1 > number2:
+    # block of code (can be any size)
+    print("1>2")
+else:
+    if number1 < number2:
+        print("1<2")
+    else:
+        print("equal")
+        print("because they have the same value")
